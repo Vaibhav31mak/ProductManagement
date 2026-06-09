ed33b39db0e3657e667ef069d1193cecfa8d8483
Vaibhav31Mak <vaibhav311004@gmail.com>
2026-06-09 13:46:28 +0530

    Configured MongoDB Driver.

---
+++ ProductManagement.Infrastructure/DependencyInjection/InfrastructureInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductManagement.Infrastructure.DependencyInjection;
+
+public static class InfrastructureInjection
+{
+    extension(IServiceCollection services)
+    {
+        public void ConfigureDatabase(IConfiguration configuration)
+        {
+            var productManagementDbSettings = configuration.GetSection("ProductManagementDatabase")
+                .Get<ProductManagementSettings>()!;
+            var client = new MongoClient(productManagementDbSettings.ConnectionString);
+            var database = client.GetDatabase(productManagementDbSettings.DatabaseName);
+
+            services.AddSingleton<IMongoDatabase>(database);
+        }
+    }
+}
--- ProductManagement.Infrastructure/GlobalUsings.cs
+++ ProductManagement.Infrastructure/GlobalUsings.cs
@@ -1,1 +1,5 @@
-﻿
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
+global using MongoDB.Driver;
+
+global using ProductManagement.Infrastructure.Data.Settings;
